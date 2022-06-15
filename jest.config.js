51ed1d5c3ea6daa8fce84fa9816238c8223335c9
Amachua <vidal@et.esiea.fr>
2022-06-15 22:28:14 +0200

    Make reproduction case for Ruleset issue

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,10 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: [
     '<rootDir>/src/__tests__/*.ts',
-  ]
+  ],
+  moduleNameMapper: {
+    '^nimma/legacy$': '<rootDir>/node_modules/nimma/dist/legacy/cjs/index.js',
+    '^nimma/fallbacks$': '<rootDir>/node_modules/nimma/dist/legacy/cjs/fallbacks/index.js',
+    '^@stoplight/spectral-ruleset-bundler/(.*)$': '<rootDir>/node_modules/@stoplight/spectral-ruleset-bundler/dist/$1'
+  }
 };
--- src/__tests__/ruleset-issue.ts
+++ src/__tests__/ruleset-issue.ts
@@ -1,9 +1,9 @@
-import { repro } from '../nimma-issue';
+import { repro } from '../ruleset-issue';
 import path from 'path';
 
-test('Nimma Issue', async (): Promise<void> => {
+test('Ruleset Issue', async (): Promise<void> => {
   const specificationPath = path.resolve(__dirname, `../../specifications/specification.yaml`);
-  const rulesetPath = path.resolve(__dirname, `../../rules/rules.yaml`);
+  const rulesetPath = path.resolve(__dirname, `../../rules/_style.yaml`);
   const capturedLog = await repro(specificationPath, rulesetPath);
 
   expect(capturedLog).toBe(true);
